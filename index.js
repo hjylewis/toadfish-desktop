582d5d23957edd091ca708574156ec2fc373f630
Henry <hlewis@uchicago.edu>
2016-06-15 00:44:10 -0400

    init electron app

---
+++ index.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const {app} = electron;
+
+const {BrowserWindow} = electron;
+
+let window;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.loadURL(`file://${__dirname}/index.html`);
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
