c6764d158c83cf6fd20a0074c74a95d857306167
Henry <hlewis@uchicago.edu>
2016-07-03 23:53:07 -0400

    somg cleanup

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const electron = require('electron');
 
 const {app} = electron;
 const {BrowserWindow} = electron;
-const {ipcMain} = electron;
 
-var request = require('request');
+require('./ipc');
 
 let window;
 
 function createWindow() {
     win = new BrowserWindow({width: 800, height: 600});
-    // win.loadURL(`file://${__dirname}/index.html`);
     if (process.env.ENV === "dev") {
       win.loadURL(`http://localhost:8000`);
     } else {
@@ -35,57 +33,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-ipcMain.on('ping', (event) => {
-  console.log('ping');
-  event.sender.send('pong');
-});
-
-var SongImport = require('./lib/songImport');
-var songImport = new SongImport();
-songImport.importSongs("/Users/hlewis/Music/iTunes/iTunes Media/Music/Rihanna/Loud/01\ S&M.m4a");
-
-ipcMain.on('song', (event, path) => {
-  SongImport.getSongData(path, (err, data) => {
-    if (err) {
-      event.sender.send('song', {
-        err: err
-      });
-    } else {
-      event.sender.send('song', {
-        confirmPath: path,
-        song: data
-      });
-    }
-  })
-});
-
-ipcMain.on('storeSongs', (event, data) => {
-  console.log(data);
-  songImport.getMetadata((err, songData) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    // TODO redo and make secure
-    songData.forEach((song) => {
-      request.post({
-          url:'http://localhost:8000/localsong/' + data.roomID + '/storeSongs',
-          form: {
-            song: JSON.stringify(song)
-          }
-        },
-        (err, httpResponse, body) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log(httpResponse.statusCode);
-          console.log(body);
-          event.sender.send('localEnabled');
-        });
-    });
-  })
-})
-
-// var SongImport = require('./lib/songImport');
-// SongImport.getSongData("/Users/hlewis/Music/iTunes/iTunes Media/Music/Rihanna/Loud/01\ S&M.m4a");
